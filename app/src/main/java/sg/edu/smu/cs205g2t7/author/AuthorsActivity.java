e9f1c0e170e28820f64572678676c70bc193819b
suo-haoran <suohaoran99@gmail.com>
2024-04-05 20:45:28 +0800

    Done

--- app/src/main/java/sg/edu/smu/cs205g2t7/author/AuthorsActivity.java
+++ app/src/main/java/sg/edu/smu/cs205g2t7/author/AuthorsActivity.java
@@ -1,8 +1,11 @@
 package sg.edu.smu.cs205g2t7.author;
 
 import android.os.Bundle;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,10 +20,22 @@ public class AuthorsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_authors);
+        hideStatusBar();
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.hide();
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void hideStatusBar() {
+        WindowInsetsController wic = getWindow().getInsetsController();
+        if (wic != null) {
+            wic.hide(WindowInsets.Type.statusBars());
+        }
     }
 }
